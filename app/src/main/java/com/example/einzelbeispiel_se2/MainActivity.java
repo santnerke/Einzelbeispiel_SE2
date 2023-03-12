27a0a60c3ee8e6ed33ef07f2371348c25d213e86
Kevin Santner <kevinsan@edu.aau.at>
2023-03-12 19:14:30 +0100

    GUI mit Button erweitert um ersetzen durch ASCII Characters zu ermöglichen

--- app/src/main/java/com/example/einzelbeispiel_se2/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel_se2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +29,30 @@ public class MainActivity extends AppCompatActivity {
         String matrikelNummer = tfNumber.getText().toString();
         new TcpConnection(this).execute(matrikelNummer);
         tfNumber.clearFocus();
+    }
+
+    public void onCalculateButtonClick(View view){
+        String matrikelNummer = tfNumber.getText().toString();
+        if (matrikelNummer.length() < 8) {
+            Toast.makeText(this, "Bitte geben Sie eine 8-stellige Matrikelnummer ein", Toast.LENGTH_SHORT).show();
+        } else {
+            updateTextField(convertToASCII(matrikelNummer));
+        }
+    }
+
+    private String convertToASCII(String matrikel) {
+        StringBuilder ascii = new StringBuilder();
+        for (int i = 0; i < matrikel.length(); i++) {
+            char c = matrikel.charAt(i);
+            if (i % 2 == 0) {
+                ascii.append(c);
+            } else {
+                int asciiValue = (int) c + 48;
+                ascii.append((char) asciiValue);
+            }
+        }
+
+        return ascii.toString();
     }
 
     public void updateTextField(String result) {
